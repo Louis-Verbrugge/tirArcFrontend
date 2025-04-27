3d9f73ca6f87f7f13127d7d17b8b11b79475e1da
Louis-Verbrugge <louvbg@gmail.com>
2025-04-27 15:33:18 +0200

    finish navBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,47 +1,153 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { gsap } from 'gsap';
 import styles from './NavBar.module.scss';
 
-import { useRef } from "react";
-import { getWidth } from "rsuite/esm/DOMHelper";
 
 export function NavBar() {
-  const [openNav, setOpenNav] = useState(false);
-
-  const handleWindowResize = () => window.innerWidth >= 960 && setOpenNav(false);
 
   const navBarRef = useRef(null);
+  
+  //const [annimNavBar, annimNavBar] = useState(null);
 
-  function changePage() {
-    gsap.to(
-      navBarRef.current,
-      { width: "44px", duration: 1, onComplete: () => {
+  const timeFirstPartAnnim = .5;
+  const timeSecondPartAnnim = .5;
 
-        gsap.to(
-          navBarRef.current,
-          { x: "-100px", duration: 1}
-        )
+  let annimNavBar = null;
+  let annimOpenEtape = null; // 0 = Etape 0, 1 = Etape 1, 2 = Etape 2
+  let annimCloseEtape = null; // 0 = Etape0 , 1 = Etape 1, 2 = Etape 2
+  let whereIsScroll = 0;
 
-      } },
-    ); 
-  }
+  let annimOpenId = null;
+  let annimCloseId = null;
 
+
+  let blockNavBarHeight = 0;
+
+
+  let test = 0;
 
 
   useEffect(() => {
-    window.addEventListener("resize", handleWindowResize);
+    blockNavBarHeight = navBarRef.current.getBoundingClientRect().height
+  }, []);
+
+  useEffect(() => {
+    let lastScrollY = window.scrollY;
+  
+    const handleScroll = () => {
+      if (window.scrollY > lastScrollY) {
+        closeNavBar();
+      } else {
+        openNavBar();
+      }
+      lastScrollY = window.scrollY;
+    };
+  
+    window.addEventListener("scroll", handleScroll);
+  
     return () => {
-      window.removeEventListener("resize", handleWindowResize);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
+
+
+  function closeNavBar() {
+
+    if (whereIsScroll != 0) {
+      whereIsScroll = 0;
+
+      if (annimNavBar != null) {
+        annimNavBar.kill(true);
+        annimNavBar = null;
+      }
+      const animationId = Date.now();
+      annimCloseId = animationId;
+    
+      if (annimOpenEtape == 2 || annimOpenEtape == null) {
+        annimCloseEtape = 1
+        annimNavBar = gsap.to(
+          navBarRef.current,
+          { width: "44px", height: "44px", duration: timeSecondPartAnnim, onComplete: () => {
+
+            if (annimCloseId == animationId && annimCloseId >= annimOpenId) {
+              annimCloseEtape = 2
+              let possitionXNavBar = navBarRef.current.getBoundingClientRect().x; 
+              annimNavBar = gsap.to(
+                navBarRef.current,
+                { x: -possitionXNavBar-200+"px", duration: timeFirstPartAnnim, onComplete: () => { annimNavBar=null } }
+              );
+            }
+          }},
+        );
+      } else {
+        // je joue la fin de l'animation
+        if (annimCloseId == animationId) {
+          annimCloseEtape = 2;
+          let possitionXNavBar = navBarRef.current.getBoundingClientRect().x; 
+          annimNavBar = gsap.to(
+            navBarRef.current,
+            { x: -possitionXNavBar-200+"px", duration: timeFirstPartAnnim, onComplete: () => { annimNavBar=null } }
+          );
+        }
+      }
+    }
+  }
+
+  function openNavBar() {
+
+    if (whereIsScroll != 1) {
+      
+      whereIsScroll = 1;
+      if (annimNavBar != null) {
+        annimNavBar.kill(true);
+        annimNavBar = null;
+      }
+      const animationId = Date.now();
+      annimOpenId = animationId;
+
+      if (annimCloseEtape == 2 || annimCloseEtape == null) {
+        annimOpenEtape = 1
+        annimNavBar = gsap.fromTo(
+          navBarRef.current,
+          { x: "-200px", width: "44px", height: "44px"},
+          { x: "0px", duration: timeFirstPartAnnim, onComplete: () => {
+
+            if (annimOpenId == animationId && annimOpenId >= annimCloseId) {
+              annimOpenEtape = 2
+              
+              annimNavBar = gsap.to(
+                navBarRef.current,
+                { width: "auto", duration: timeSecondPartAnnim, onComplete: () => { annimNavBar=null } }
+              );
+            }
+          } },
+        ); 
+      } else {
+        // je joue la fin de l'animation
+        if (annimOpenId == animationId) {
+          annimOpenEtape = 2;
+          annimNavBar = gsap.to(
+            navBarRef.current,
+            { width: "auto", duration: timeSecondPartAnnim, onComplete: () => { annimNavBar=null } }
+          );
+        }
+      }
+    }
+  }
+
+  useEffect(() => {
+    openNavBar()
+  }, []);
+
+
   return (
-      <nav className={styles.navBar} >
+      <nav className={styles.navBar}>
         <div className={styles.content} ref={navBarRef} id="navBar">
           <p>Tir à l'arc Cysoing</p>
           <div className={styles.nav_icon}>
 
-            <p onClick={() => changePage()}>News</p>
+            <p>News</p>
 
             <p>Qui sommes-nous ?</p>
             
@@ -49,10 +155,8 @@ export function NavBar() {
             
             <p>Contact</p> 
 
+        
 
-            {console.log("sddddddddddddddddddddddddddddddddddddddddddddddddddddddd 11")}
-            {console.log(document.getElementById("navBar"))}
-            {console.log("sddddddddddddddddddddddddddddddddddddddddddddddddddddddd 22")}
 
           </div>
         </div>
